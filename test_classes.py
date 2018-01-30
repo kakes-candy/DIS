3c39b5b14662bab8e85a3a93c1d5fa6fefd872a2
niels <niels.kakes@gmail.com>
2018-01-30 10:42:10 +0100

    added first tests

--- test_classes.py
+++ test_classes.py
@@ -4,9 +4,14 @@ from classes import Patient
 
 
 class testPatient(unittest.TestCase):
+    def setUp(self):
+        self.patient1 = Patient(_1430 = '73730802')
 
-
-
+    def test_patient_write_to_string(self):
+        # de lengte van de output kan worden afgeleid uit de format_definitions
+        specced_length = int(self.patient1.format_definitions[-1]['Eind'])
+        length_output = len(self.patient1.write_to_string())
+        self.assertEqual(specced_length, length_output)
 
 
 
