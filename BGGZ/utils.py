a83cc9c461dd0fd0208af36d8c2246595deb3d55
niels <niels.kakes@gmail.com>
2019-05-16 08:48:59 +0200

    functie om definities vanuit excel te lezen

---
+++ BGGZ/utils.py
@@ -0,0 +1,30 @@
+import pprint, xlrd
+
+
+path = (
+    "/home/niels/workspace/Python/DIS/BGGZ/Documentatie/bggz bestanden definities.xlsx"
+)
+
+
+def read_format_defs(path, sheet):
+
+    wb = xlrd.open_workbook(path)
+
+    sheet = wb.sheet_by_name(sheet)
+
+    keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
+
+    dict_list = []
+    for row_index in range(1, sheet.nrows):
+        d = {
+            keys[col_index]: str(sheet.cell(row_index, col_index).value).replace(
+                ".0", ""
+            )
+            for col_index in range(sheet.ncols)
+        }
+        dict_list.append(d)
+
+    return dict_list
+
+
+pprint.pprint(read_format_defs(path=path, sheet="pakbon.txt"))
