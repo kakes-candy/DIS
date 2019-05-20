e4036e2eb500dfa22b3eb734a19c69aef7b0f779
niels <niels.kakes@gmail.com>
2019-05-20 08:45:28 +0200

    eerste  check op classes

--- BGGZ/test2.py
+++ BGGZ/test2.py
@@ -1,29 +1,18 @@
 #
-# from classes import Patient, Pakbon, Zorgtraject, DBCTraject, Tijdschrijven, Diagnose, PakbonTekst
-# from collections import OrderedDict
-# import csv, os
-# from pprint import pprint
-#
-#
-#
-# bronmap = 'Test rapportages/16_52/DIS_GGZ_DBC_TEST_080_73730802_01_20190312_01/'
-#
-#
-# pb = Pakbon()
-#
-#
-# with open(os.path.join(bronmap, 'patient.txt'), 'r' ) as f_patient:
-#     patient = Patient.from_string(f_patient.readline())
-#     # for line in f_patient:
-#     #     patient = Patient.from_string(line)
-#     #     pb.add_patient(patient)
-#
-#
-# print(patient.verplichte_velden())
+from classes import Patient, Behandeltraject, GeleverdZorgprofiel, PakbonTekst
+import sys
 
 
+p = Patient()
+# print(p.help())
+
+b = Behandeltraject()
+# print(b.help())
 
 
-getal = '-01253'
+gz = GeleverdZorgprofiel()
+# print(gz.help())
 
-print(int(getal))
+pbt = PakbonTekst()
+print(pbt.help())
+
