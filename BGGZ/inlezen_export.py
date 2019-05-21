f1b55f2e37dfb3413a0c7c46423e802ee64ab6a8
Niels Kakes Admin <hskadminnkakes@HSKGROEP.local>
2019-05-21 16:47:09 +0200

    inlezen export geschikt voor BGGZ

--- BGGZ/inlezen_export.py
+++ BGGZ/inlezen_export.py
@@ -1,72 +1,59 @@
-from classes import Patient, Pakbon, Zorgtraject, DBCTraject, Tijdschrijven, Diagnose
+from classes import Patient, Pakbon, Behandeltraject, GeleverdZorgprofiel
 from collections import OrderedDict
 import csv, os
 
 
-bronmap = 'Test rapportages/11_52/DIS_GGZ_DBC_TEST_080_73730802_01_20190320_01'
+bronmap = "BGGZ/Test_rapportages/Bggz-201412-Created-20190509_135234/DIS_GBG_TRJ_TEST_020_73730802_01_20190509_01"
 
+print(os.getcwd())
 
 # Submap voor ingelezen bestanden aanmaken in bronmap
 try:
-    os.mkdir(os.path.join(bronmap, 'ingelezen'))
+    os.mkdir(os.path.join(bronmap, "ingelezen"))
 except FileExistsError:
-    print('Directory not created')
+    print("Directory not created")
 
 
-with open(os.path.join(bronmap, 'pakbon.txt'), 'r' ) as f_pakbon:
+with open(os.path.join(bronmap, "PAKBON.txt"), "r") as f_pakbon:
     p = f_pakbon.readline()
     # print(Pakbon.from_string(p).write_to_dict())
 
-with open(os.path.join(bronmap, 'patient.txt'), 'r' ) as f_patient:
+with open(os.path.join(bronmap, "PATIENT.txt"), "r") as f_patient:
 
-    headers_patient =  [x['Naam'] for x in Patient().format_definitions]
-    with open(os.path.join(bronmap, 'ingelezen', 'patient.csv'), 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, delimiter = ';', fieldnames = headers_patient)
+    headers_patient = [x["Naam"] for x in Patient().format_definitions]
+    with open(
+        os.path.join(bronmap, "ingelezen", "patient.csv"), "w", newline=""
+    ) as outfile:
+        writer = csv.DictWriter(outfile, delimiter=";", fieldnames=headers_patient)
         writer.writeheader()
         for line in f_patient:
             patient = Patient.from_string(line)
             writer.writerow(patient.write_to_dict())
 
 
-with open(os.path.join(bronmap, 'zorgtraject.txt'), 'r' ) as f_zorgtraject:
+with open(os.path.join(bronmap, "BEHANDELTRAJECT.txt"), "r") as f_behandeltraject:
 
-    headers_zorgtraject =  [x['Naam'] for x in Zorgtraject().format_definitions]
-    with open(os.path.join(bronmap, 'ingelezen', 'zorgtraject.csv'), 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, delimiter = ';', fieldnames = headers_zorgtraject)
+    headers_behandeltraject = [x["Naam"] for x in Behandeltraject().format_definitions]
+    with open(
+        os.path.join(bronmap, "ingelezen", "dbctraject.csv"), "w", newline=""
+    ) as outfile:
+        writer = csv.DictWriter(
+            outfile, delimiter=";", fieldnames=headers_behandeltraject
+        )
         writer.writeheader()
-        for line in f_zorgtraject:
-            zorgtraject = Zorgtraject.from_string(line)
-            writer.writerow(zorgtraject.write_to_dict())
-
-
-with open(os.path.join(bronmap, 'dbc_traject.txt'), 'r' ) as f_dbctraject:
-
-    headers_dbctraject =  [x['Naam'] for x in DBCTraject().format_definitions]
-    with open(os.path.join(bronmap, 'ingelezen', 'dbctraject.csv'), 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, delimiter = ';', fieldnames = headers_dbctraject)
-        writer.writeheader()
-        for line in f_dbctraject:
-            dbctraject = DBCTraject.from_string(line)
+        for line in f_behandeltraject:
+            dbctraject = Behandeltraject.from_string(line)
             writer.writerow(dbctraject.write_to_dict())
 
 
-with open(os.path.join(bronmap, 'geleverd_zorgprofiel_tijdschrijven.txt'), 'r' ) as f_tijdschrijven:
+with open(os.path.join(bronmap, "GELEVERD_ZORGPROFIEL.txt"), "r") as f_tijdschrijven:
 
-    headers_tijdschrijven =  [x['Naam'] for x in Tijdschrijven().format_definitions]
-    with open(os.path.join(bronmap, 'ingelezen', 'tijdschrijven.csv'), 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, delimiter = ';', fieldnames = headers_tijdschrijven)
+    headers_zorgprofiel = [x["Naam"] for x in GeleverdZorgprofiel().format_definitions]
+    with open(
+        os.path.join(bronmap, "ingelezen", "geleverd_zorgprofiel.csv"), "w", newline=""
+    ) as outfile:
+        writer = csv.DictWriter(outfile, delimiter=";", fieldnames=headers_zorgprofiel)
         writer.writeheader()
         for line in f_tijdschrijven:
-            tijdschrijven = Tijdschrijven.from_string(line)
-            writer.writerow(tijdschrijven.write_to_dict())
-
-
-with open(os.path.join(bronmap, 'diagnose.txt'), 'r' ) as f_diagnose:
-
-    headers_diagnose =  [x['Naam'] for x in Diagnose().format_definitions]
-    with open(os.path.join(bronmap, 'ingelezen', 'diagnose.csv'), 'w', newline='') as outfile:
-        writer = csv.DictWriter(outfile, delimiter = ';', fieldnames = headers_diagnose)
-        writer.writeheader()
-        for line in f_diagnose:
-            diagnose = Diagnose.from_string(line)
-            writer.writerow(diagnose.write_to_dict())
+            zorgprofiel = GeleverdZorgprofiel.from_string(line)
+            writer.writerow(zorgprofiel.write_to_dict())
